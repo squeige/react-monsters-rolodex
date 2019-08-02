8964757800d00653021c4320a3ad329655fe50e3
Luigi <virtual.employee@gmail.com>
2019-08-02 15:10:07 -0600

    Updates

---
+++ precache-manifest.d23cf0125e5158f5f9a090b462acf891.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7351c119b3e5f467823f746b7edd076",
+    "url": "/squeige/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "56b101b35f128173c4b7",
+    "url": "/squeige/react-monsters-rolodex/static/css/main.526f33f8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/squeige/react-monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "56b101b35f128173c4b7",
+    "url": "/squeige/react-monsters-rolodex/static/js/main.dba8c9bf.chunk.js"
+  },
+  {
+    "revision": "b75e65a0102cd2edc04f",
+    "url": "/squeige/react-monsters-rolodex/static/js/runtime~main.e297d9d5.js"
+  }
+]);
